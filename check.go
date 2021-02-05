8a1817776fafa57b2660e7fa263cd646b49c5fac
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-02-05 07:46:12 +0100

    refactor: move the Check interface back to the root module
    Fundamentally, the Check interface belongs to the root
    package as it's the main consumer of that contract.
    
    It also felt wrong to depend on the checks package
    as it contains implementations of that interface.
    
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ check.go
@@ -0,0 +1,11 @@
+package gosundheit
+
+// Check is the API for defining health checks.
+// A valid check has a non empty Name() and a check (Execute()) function.
+type Check interface {
+	// Name is the name of the check.
+	// Check names must be metric compatible.
+	Name() string
+	// Execute runs a single time check, and returns an error when the check fails, and an optional details object.
+	Execute() (details interface{}, err error)
+}
--- config.go
+++ config.go
@@ -2,14 +2,12 @@ package gosundheit
 
 import (
 	"time"
-
-	"github.com/AppsFlyer/go-sundheit/checks"
 )
 
 // Config defines a health Check and it's scheduling timing requirements.
 type Config struct {
 	// Check is the health Check to be scheduled for execution.
-	Check checks.Check
+	Check Check
 	// ExecutionPeriod is the period between successive executions.
 	ExecutionPeriod time.Duration
 	// InitialDelay is the time to delay first execution; defaults to zero.
